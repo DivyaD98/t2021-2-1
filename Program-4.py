cf8e5d6e4db8bd1570f7f9a999db665dfcefd97d
DivyaD98 <102279533+DivyaD98@users.noreply.github.com>
2022-09-26 21:58:53 +0530

    Update Program-4.py

--- Program-4.py
+++ Program-4.py
@@ -6,11 +6,15 @@
 '''
 
 
-arr = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
-dic = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
-
-for num in arr:
-    for i in range(1, 10):
-        if num % i == 0:
-            dic[i] += 1
-print(dic)
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+data = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
+lst = []
+count = 0
+for i in a:
+    for j in data:
+        if j % i == 0:
+            count += 1
+    lst.append(count)
+    count = 0
+result = dict(zip(a, lst))
+print(result)
