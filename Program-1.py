2a89ea5c46a5f68baf5afc8424624934586a9e80
DivyaD98 <102279533+DivyaD98@users.noreply.github.com>
2022-09-26 20:24:34 +0530

    Update Program-1.py

--- Program-1.py
+++ Program-1.py
@@ -6,42 +6,19 @@ Create a small calculator which performs operations such as Addition, Subtractio
 
 #Calculator
 
-def add(a, b):
-    return a + b
+a = float(input("Enter 1st number :"))
+b = float(input("Enter 2nd number :"))
+type_of_operation = str(input("Enter type of operation : +--> Add, \'-\'--> Subtract, *--> Multiply, /--> Divide "))
 
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    return a / b
-
-print("Select any operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    choice = (input("Enter choice(1,2,3,4): "))
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter the first value: "))
-        num2 = float(input("Enter the second value: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", sub(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", mul(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", div(num1, num2))
-        break
+if type_of_operation == "+":
+    print(a, '+', b, '=', a + b)
+elif type_of_operation == "-":
+    print(a, '-', b, '=', a - b)
+elif type_of_operation == "*":
+    print(a, '*', b, '=', a * b)
+elif type_of_operation == "/":
+    if b != 0:
+        print(a, '/', b, '=', a / b)
     else:
-        print("Invalid Input")
+        print("Invalid input")
 
