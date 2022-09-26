15a383ff118db5e8e519fddf3b324ac84780c02d
DivyaD98 <102279533+DivyaD98@users.noreply.github.com>
2022-09-26 17:26:49 +0530

    All programs are in Python

--- Program-1.py
+++ Program-1.py
@@ -1,16 +1,42 @@
 
 #Calculator
 
-def calculator(a, b):
-    add = a + b
-    sub = a - b
-    mul = a * b
-    div = a / b
-    return add, sub, mul, div
-a = int(input('Enter first number'))
-b = int(input('Enter second number'))
-p, q, r, s = calculator(a, b)
-print(p,end=' ')
-print(q,end=' ')
-print(r,end=' ')
-print(s,end=' ')
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print("Select any operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = (input("Enter choice(1,2,3,4): "))
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter the first value: "))
+        num2 = float(input("Enter the second value: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", sub(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", mul(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", div(num1, num2))
+        break
+    else:
+        print("Invalid Input")
+
