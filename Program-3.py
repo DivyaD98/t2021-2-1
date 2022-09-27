58abb78fa5fe9a2f2d58b7de603037e8fbe97f55
DivyaD98 <102279533+DivyaD98@users.noreply.github.com>
2022-09-27 11:52:37 +0530

    Update Program-3.py

--- Program-3.py
+++ Program-3.py
@@ -1,26 +1,12 @@
 # With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
-'''
-    Output: (examples)
-        1) input a = 1, then output : 1
-        2) input a = 2, then output : 1
-        3) input a = 3, then output : 1, 3, 5
-        4) input a = 4, then output : 1, 3, 5
-        5) input a = 5, then output : 1, 3, 5, 7, 9
-        6) input a = 6, then output : 1, 3, 5, 7, 9
-        .
-        .
-        7) input a = x, then output : 1, 3, 5, 7, .......
-  '''
 
 
-n = int(input('Enter a number'))
-if n % 2 == 0:
-    n = n - 1
-lst = []
+
+a = int(input('Enter a number'))
+if a % 2 == 0:
+    a -=1
 i = 1
-while len(lst) < n:
-    if i % 2 != 0:
-        lst.append(i)
-    i = i + 1
-print(lst)
- 
+while a > 0:
+    print(i,end=" ")
+    i = i + 2
+    a = a - 1
