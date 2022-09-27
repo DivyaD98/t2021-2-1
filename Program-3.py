b4f61d56f12ee47cff2812e6adc9327cc93976ec
DivyaD98 <102279533+DivyaD98@users.noreply.github.com>
2022-09-27 11:32:16 +0530

    Update Program-3.py

--- Program-3.py
+++ Program-3.py
@@ -17,10 +17,10 @@ n = int(input('Enter a number'))
 if n % 2 == 0:
     n = n - 1
 lst = []
-num = 1
+i = 1
 while len(lst) < n:
-    if num % 2 != 0:
-        lst.append(num)
-    num = num + 1
+    if i % 2 != 0:
+        lst.append(i)
+    i = i + 1
 print(lst)
  
